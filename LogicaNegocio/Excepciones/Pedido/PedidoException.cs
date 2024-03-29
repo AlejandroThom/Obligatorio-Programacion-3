34a4b1d3ed77df8499d3c9ab5a2abcd3ae0891c9
SantiagoMuslera <santiagomuslerla@gmail.com>
2024-03-29 18:36:39 -0300

    + Se crearon las excepciones de pedido y usuario. + Se creo el controllador de Cliente y el ClienteViewModel. No pude agregar la vista de create porque me da un error de compilacion que no se solucionar.

---
+++ LogicaNegocio/Excepciones/Pedido/PedidoException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Excepciones.Pedido
+{
+    internal class PedidoException:Exception
+    {
+        public PedidoException() { }
+
+        public PedidoException(string message) : base(message) { }
+
+        public PedidoException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Obligatorio Programacion 3/Models/ClienteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LogicaNegocio.EntidadesNegocio;
+
+namespace Obligatorio_Programacion_3.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+        public string RazonSocial { get; set; }
+        public string RUT { get; set; }
+
+        public Direccion Direccion { get; set; }
+
+        public ClienteViewModel() { }
+    }
+}
