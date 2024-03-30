3d2ecccf1835a1d74ec732b51d6b612b61431502
Alejandro <theicehornet1@gmail.com>
2024-03-30 16:56:43 -0500

    Update: métodos de validacion de articulo.

--- LogicaNegocio/EntidadesNegocio/Articulo.cs
+++ LogicaNegocio/EntidadesNegocio/Articulo.cs
@@ -1,4 +1,6 @@
-﻿using LogicaNegocio.Interfaces;
+﻿using LogicaNegocio.Excepciones.Articulo;
+using LogicaNegocio.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,11 +10,15 @@ using System.Threading.Tasks;
 
 namespace LogicaNegocio.EntidadesNegocio
 {
+    [Index(nameof(Nombre),IsUnique = true)]
     public class Articulo : IValidable
     {
         public int Id { get; set; }
+
         public string Nombre { get; set; }
 
+
+        [MinLength(5)]
         /// <summary>
         /// La descripcion debe tener un minimo de 5 caracteres
         /// </summary>
@@ -28,9 +34,67 @@ namespace LogicaNegocio.EntidadesNegocio
 
         public Articulo() { }
 
+        /// <summary>
+        /// Verifica que el articulo sea valido.
+        /// </summary>
         public void Validar()
         {
-            throw new NotImplementedException();
+            ValidarNombre();
+            ValidarDescripcion();
+            ValidarCodigo();
+            ValidarPrecioPublico();
+            ValidarStock();
+        }
+
+        /// <summary>
+        /// Verifica que el nombre no sea nulo o vacío
+        /// </summary>
+        /// <exception cref="ArticuloException"></exception>
+        private void ValidarNombre()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+                throw new ArticuloException("El nombre del articulo no puede ser vacío o nulo");
+        }
+        /// <summary>
+        /// Valida que la descripcion tenga al menos 5 caracteres
+        /// </summary>
+        /// <exception cref="ArticuloException"></exception>
+        private void ValidarDescripcion()
+        {
+            if(Descripcion.Length <5)
+                throw new ArticuloException("La descripcion del articulo debe tener al menos 5 caracteres.");
+
+        }
+        /// <summary>
+        /// Valida que el codigo tenga los 13 caracteres correspondientes
+        /// </summary>
+        /// <exception cref="ArticuloException"></exception>
+        private void ValidarCodigo()
+        {
+            if(Codigo.Length  != 13)
+                throw new ArticuloException("El codigo del articulo debe tener solamente 13 caracteres.");
+
+        }
+        /// <summary>
+        /// Valida que el precio mayor a 0
+        /// </summary>
+        /// <exception cref="ArticuloException"></exception>
+        private void ValidarPrecioPublico()
+        {
+            if (PrecioPublico  <= 0)
+                throw new ArticuloException("El precio del articulo no puede ser negativo o gratis.");
+
+        }
+
+        /// <summary>
+        /// Valida que el Stock sea mayor a 0
+        /// </summary>
+        /// <exception cref="ArticuloException"></exception>
+        private void ValidarStock()
+        {
+            if (Stock <= 0)
+                throw new ArticuloException("El stock del articulo no puede ser negativo o 0.");
+
         }
     }
 }
