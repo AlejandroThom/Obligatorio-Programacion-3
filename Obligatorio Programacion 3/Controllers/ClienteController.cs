fc9ce455d214f7d6bed15ef4bf622e791b792d6e
Alejandro <theicehornet1@gmail.com>
2024-04-05 15:06:54 -0500

    Update:Agregado controladores para cliente, articulo y pedido

---
+++ Obligatorio Programacion 3/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Obligatorio_Programacion_3.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
