1e73bd29d633c265830d0d98eb4b8b129c94a5fb
SantiagoMuslera <santiagomuslerla@gmail.com>
2024-04-16 14:57:30 -0300

    Update: Se agrega en program las variables de sesion

--- Obligatorio Programacion 3/Program.cs
+++ Obligatorio Programacion 3/Program.cs
@@ -85,6 +85,16 @@ namespace Obligatorio_Programacion_3
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            //Se agrega el uso de variables de sesion
+            builder.Services.AddDistributedMemoryCache();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             
 
             var app = builder.Build();
@@ -103,6 +113,10 @@ namespace Obligatorio_Programacion_3
             app.UseRouting();
 
             app.UseAuthorization();
+
+            //Indicacion para que utilice la variable de sesion.
+
+            app.UseSession();
 
             app.MapControllerRoute(
                 name: "default",
