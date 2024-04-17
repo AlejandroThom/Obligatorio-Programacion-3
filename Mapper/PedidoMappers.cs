1ffce4854769d0f747d33c13b4ff2454b47a9803
Alejandro <theicehornet1@gmail.com>
2024-04-17 08:44:42 -0500

    Update: nuevo método de PedidoMappers para convertir una lista de pedidos a una de PedidoListadoDTO

--- Mapper/PedidoMappers.cs
+++ Mapper/PedidoMappers.cs
@@ -31,6 +31,18 @@ namespace Mapper
                 PrecioTotal = pedido.PrecioPedidoFinal
             };
         }
+        /// <summary>
+        /// Retorna una lista de PedidosListaDTO a partir de una lista de Pedido
+        /// </summary>
+        /// <param name="pedis">Lista con los pedidos a convertir a DTO</param>
+        /// <returns>Retorna una lista de PedidoListaDTO</returns>
+        /// <exception cref="Exception"></exception>
+        public static IEnumerable<PedidoListadoDTO> ToListaDto(IEnumerable<Pedido> pedis)
+        {
+            if (pedis == null)
+                throw new Exception("El objeto no puede ser nulo");
+            return pedis.Select(p => ToDto(p));
+        }
 
     }
 }
