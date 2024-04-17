8fa6a9ddf5648a8d472d3c2fe311cb19acc68b90
Alejandro <theicehornet1@gmail.com>
2024-04-17 07:55:31 -0500

    Update: Se creo PedidoDTO y se puso sus propiedades y a ArticuloDTO tambien se le puso sus propiedades.

--- DTO/ArticuloDTO.cs
+++ DTO/ArticuloDTO.cs
@@ -1,7 +1,14 @@
-﻿namespace DTO
+﻿using LogicaNegocio.ValueObjects.ArticulosVO;
+
+namespace DTO
 {
     public class ArticuloDTO
     {
-
+        public int Id { get; set; }
+        public string NombreArticulo { get; set; }
+        public string DescripcionArticulo { get; set; }
+        public decimal PrecioPublico { get; set; }
+        public string CodigoArticulo { get; set; }
+        public int Stock { get; set; }
     }
 }
---
+++ DTO/PedidoDTO.cs
@@ -0,0 +1,16 @@
+﻿using LogicaNegocio.EntidadesNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PedidoDTO
+    {
+        public DateTime FechaEntrega { get; set; }
+        public Cliente Cliente { get; set; }
+        public decimal PrecioTotal { get; set; }
+    }
+}
