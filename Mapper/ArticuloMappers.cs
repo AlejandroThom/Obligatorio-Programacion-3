df76582e2cbdae00906eba21f7fae6a373a23090
Alejandro <theicehornet1@gmail.com>
2024-04-17 11:18:36 -0500

    Update: Hecho los métodos de ArticuloMappers para pasar de Articulo a ArticuloListadoDTO

---
+++ Mapper/ArticuloMappers.cs
@@ -0,0 +1,32 @@
+﻿using DTO;
+using LogicaNegocio.EntidadesNegocio;
+
+namespace Mapper
+{
+    public class ArticuloMappers
+    {
+        public static ArticuloListadoDTO ToDto(Articulo articulo)
+        {
+            if (articulo == null)
+                throw new ArgumentNullException();
+            return new ArticuloListadoDTO()
+            {
+                Id = articulo.Id,
+                CodigoArticulo = articulo.CodigoArticulo.Codigo,
+                DescripcionArticulo = articulo.DescripcionArticulo.Descripcion,
+                NombreArticulo = articulo.NombreArticulo.Nombre,
+                Stock = articulo.Stock,
+                PrecioPublico = articulo.PrecioPublico
+            };
+        }
+        public static IEnumerable<ArticuloListadoDTO> ToListaDto(IEnumerable<Articulo> articulos)
+        {
+            if (articulos == null)
+                throw new ArgumentNullException();
+            return articulos.Select(ar => ToDto(ar));
+        }
+
+    }
+
+
+}
