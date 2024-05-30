1ad5a7262f1c5dee599b3f9812e37d8c66388ce8
LuisThom <theicehornet1@gmail.com>
2024-05-30 16:23:16 -0300

    Update:TipoMovimiento Mapper Se realizo un mapeador de la clase TipoMovimiento a DTO y viceversa.

---
+++ DTO/TipoMovimientoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TipoMovimientoDTO
+    {
+        public int Id { get; set; }
+        [StringLength(80,MinimumLength =4,ErrorMessage ="El nombre del tipo de movimiento debe tener al menos 4 caracteres.")]
+        public String NombreMovimiento { get; set; }
+    }
+}
---
+++ Mapper/TipoMovimientoMappers.cs
@@ -0,0 +1,49 @@
+﻿using DTO;
+using LogicaNegocio.EntidadesNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class TipoMovimientoMappers
+    {
+        public static TipoMovimientoDTO ToDto(TipoMovimiento item)
+        {
+            if (item == null)
+                throw new Exception("El objeto no puede ser nulo");
+            return new TipoMovimientoDTO
+            {
+                Id = item.Id,
+                NombreMovimiento = item.NombreMovimiento
+            };
+        }
+
+        public static IEnumerable<TipoMovimientoDTO> ToListadoDto(IEnumerable<TipoMovimiento> items)
+        {
+            if (items == null)
+                throw new Exception("El objeto no puede ser nulo");
+            return items.Select(ToDto);
+        }
+
+        public static TipoMovimiento ToTipoMovimiento(TipoMovimientoDTO item)
+        {
+            if (item == null)
+                throw new Exception("El objeto no puede ser nulo");
+            return new TipoMovimiento
+            {
+                NombreMovimiento = item.NombreMovimiento
+            };
+        }
+
+        public static IEnumerable<TipoMovimiento> ToListadoTipoMovimiento(IEnumerable<TipoMovimientoDTO> items)
+        {
+            if (items == null)
+                throw new Exception("El objeto no puede ser nulo");
+            return items.Select(ToTipoMovimiento);
+        }
+
+    }
+}
